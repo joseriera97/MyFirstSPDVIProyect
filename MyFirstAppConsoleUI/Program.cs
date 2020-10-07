b483f47538c1581a983571475a690aca4ed6e580
joseriera97 <josealbertoriera@paucasesnovescifp.cat>
2020-10-07 19:26:53 +0200

    Añadida la Ventana para un Form

---
+++ MyFirstAppConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstAppConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /** cw doble tap para escribir Console.Write**/
+            Console.WriteLine("Hello SPVDI 2020 - 2021");//Escribir en consola
+            Console.ReadKey();
+
+        }
+    }
+}
